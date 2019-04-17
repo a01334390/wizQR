0e79b3966dc05a14c2b8ae60fa557a20d950eb3e
Fernando Martin Garcia Del Angel <a01334390@itesm.mx>
2019-04-17 12:45:44 -0500

    Added initial project

---
+++ wizQR/wizQR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wizQR
+//
+//  Created by Fernando Martin Garcia Del Angel on 4/17/19.
+//  Copyright © 2019 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
